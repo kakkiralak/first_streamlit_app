0631943db2f6e0194aa89df94551119201bed513
kakkiralak <113895047+kakkiralak@users.noreply.github.com>
2022-09-19 17:28:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+streamlit.title('Breakfast Menu')
+
+streamlit.title('Omega 3 & blueberry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled Free Range Egg')
