48dc5c654fa29c9606afb60ed1ea03471f846349
kakkiralak <113895047+kakkiralak@users.noreply.github.com>
2022-09-30 14:26:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import snowflake.connector
 import requests
-import urlerror
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
